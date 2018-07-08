6933a5ae5cb6445f28fe942417f1343fc98d6a78
dabarkster <craighirata@gmail.com>
2018-07-07 21:30:13 -1000

    new

--- Python/validator.py
+++ Python/validator.py
@@ -14,6 +14,7 @@ broker="192.168.56.220"
 port=1883
 
 photo_path = '/home/pi/Pictures/'
+music_path = "/home/pi/Music/"
 font_path  = "/home/pi/escapee-validator/Fonts/"
 sound_path = "/home/pi/escapee-validator/Sounds/"
 
@@ -29,9 +30,10 @@ test_img = photo_path + "test.jpg"
 img = pygame.image.load(test_img)
 #screen = pygame.display.set_mode((0,0));
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
-screen = pygame.display.set_mode((640, 480), pygame.NOFRAME);
-area_text = pygame.Rect(0,0,400,400)
-textBox = screen.subsurface(area_text)
+screen = pygame.display.set_mode((1024, 1024), pygame.NOFRAME)
+photo_area = pygame.Rect(0,0,400,400)
+text_area = pygame.Rect(0,0,800,400)
+textBox = screen.subsurface(text_area)
 textBox.fill(white)
 
 
@@ -40,19 +42,32 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 def message_display(text):
+    screen.fill(pygame.Color("Black"), text_area)
+    pygame.display.update()
     term = font_path + 'saucer.ttf'
     #os.path.exists(term)
-    largeText = pygame.font.Font(term, 115)
+    largeText = pygame.font.Font(term, 40)
     TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((area_text.width/2),(area_text.height/2))
-    status = screen.blit(TextSurf,TextRect.center)
+    #TextRect.center = ((text_area.width/2),(text_area.height/2))
+    #status = screen.blit(TextSurf,TextRect.center)
+    status = screen.blit(TextSurf, TextRect)
     pygame.display.update()
-    time.sleep(2)
 
+def play_music(music_file):
+    pygame.mixer.music.load(music_file)
+    pygame.mixer.music.play(-1)  #-1 = loop forever
+    #pygame.mixer.music.set_volume(0.5)
 
 def main():
-	message_display("Pass...")
 
+    #message_display("Place sample into validator...")
+    #time.sleep(1)
+    #message_display("Press start button...")
+    #time.sleep(1)
+    music_file = music_path + "analyze.mp3"
+    play_music(music_file)
+    while True:
+        pass
 
 main()
 quit()
@@ -62,11 +77,10 @@ pin13 = board.get_pin('d:5:o')
 
 
 while True:
-	time.sleep(5)
-	print("on")
+    time.sleep(5)
+    print("on")
 
-	pin13.write(1)
-
-	time.sleep(5)
-	print("off")
-	pin13.write(0)
+    pin13.write(1)
+    time.sleep(5)
+    print("off")
+    pin13.write(0)
