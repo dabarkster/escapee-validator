9e089ce8b6330865d1f85997d86c10bceb347285
dabarkster <craighirata@gmail.com>
2018-07-07 18:15:29 -1000

    new

--- Python/validator.py
+++ Python/validator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import pyfirmata
 import pygame
 import random
@@ -12,7 +13,7 @@ board = pyfirmata.Arduino('/dev/ttyACM0')
 broker="192.168.56.220"
 port=1883
 
-photo_path = '/home/pi/Pictures'
+photo_path = '/home/pi/Pictures/'
 font_path  = "/home/pi/escapee-validator/Fonts/"
 sound_path = "/home/pi/escapee-validator/Sounds/"
 
@@ -24,15 +25,37 @@ red = (255,0,0)
 
 pygame.init()
 print("pypy")
-img = pygame.image.load("test.jpg")
+test_img = photo_path + "test.jpg"
+img = pygame.image.load(test_img)
 #screen = pygame.display.set_mode((0,0));
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 screen = pygame.display.set_mode((640, 480), pygame.NOFRAME);
+area_text = pygame.Rect(0,0,400,400)
+textBox = screen.subsurface(area_text)
+textBox.fill(white)
 
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, red)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    term = font_path + 'saucer.ttf'
+    #os.path.exists(term)
+    largeText = pygame.font.Font(term, 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((area_text.width/2),(area_text.height/2))
+    status = screen.blit(TextSurf,TextRect.center)
+    pygame.display.update()
+    time.sleep(2)
 
 
+def main():
+	message_display("Pass...")
 
+
+main()
+quit()
 
 pin13 = board.get_pin('d:5:o')
 
