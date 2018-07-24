c35d326d0bc1068067ed992019e528aa0ca8e81e
dabarkster <craighirata@gmail.com>
2018-07-24 05:48:51 -1000

    Update meteor.ino

--- Arduino/meteor.ino
+++ Arduino/meteor.ino
@@ -1,3 +1,43 @@
+#include "FastLED.h"
+#define NUM_LEDS 60 
+CRGB leds[NUM_LEDS];
+#define PIN 6 
+
+void setup()
+{
+  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+}
+
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H 
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue); 
+  }
+  showStrip();
+
 void loop() {
   meteorRain(0xff,0xff,0xff,10, 64, true, 30);
 }
