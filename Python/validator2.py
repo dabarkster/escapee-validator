b01b2899fb4fac3ca9028c7563da66767c92081d
dabarkster <craighirata@gmail.com>
2018-07-24 20:17:18 -1000

    new

--- Python/validator2.py
+++ Python/validator2.py
@@ -5,7 +5,9 @@ import pyfirmata
 import pygame
 import random
 import time
-import paho.mqtt.client as paho
+#import paho.mqtt.client as paho
+import paho.mqtt.client as mqtt #import the client1
+
 from subprocess import Popen
 from omxplayer.player import OMXPlayer
 from time import sleep
@@ -19,7 +21,7 @@ video_path = "/home/pi/escapee-validator/Videos/"
 
 ######################## PYFirmata Initialize ########################
 board          = pyfirmata.Arduino('/dev/ttyACM0')
-#pin_keyswitch  = board.get_pin('d:1:i')
+#pin_keyswitch  = board.get_pin('d:14:i')
 pin_bigbutt    = board.get_pin('d:2:i')
 pin_plas1      = board.get_pin('d:3:o')
 pin_plas2      = board.get_pin('d:4:o')
@@ -34,51 +36,6 @@ pin_fog        = board.get_pin('d:11:o')
 pin_arm        = board.get_pin('d:12:o')
 
 pin = board.get_pin('d:13:o')
-
-######################## PY Initialize ########################
-
-pygame.init()
-pygame.font.init()
-print("Validator!")
-
-screen_width = 1000
-screen_height = 1280
-photo_size = 400
-photo_ratio = 536/820
-photo_x = 200
-photo_y = 200
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-
-screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME);
-area_photo = pygame.Rect(200,200,600,811)
-photo_box = screen.subsurface(area_photo)
-area_text = pygame.Rect(area_photo.width + 50, photo_y + 50, photo_x + 640, photo_y + 400)
-#print(screen)
-#print(area_text)
-textBox = screen.subsurface(area_text)
-textBox.fill(white)
-
-######################## MQTT Initialize ########################
-topic        = "escapee/validator"
-topicAll     = topic + "/#"
-topicStatus  = topic + "/status"
-topicCommand = topic + "/command"
-topicTimer   = topic + "/timer"
-
-broker="192.168.56.220"
-port=1883
-
-client.on_connect = on_connect
-client.message_callback_add(topicCommand, on_message_command)
-client.on_message = on_message
-client.connect(broker_address, 1883, 60) #connect to broker
-client.loop_start()
-client.publish(topicStatus,"Starting")#publish
-time.sleep(1) #slight delay to show starting status
-client.publish(topicStatus,"Waiting")
-
 
 ######################## PY Functions ########################
 
@@ -213,6 +170,51 @@ def on_message_command(client, userdata, msg):
     if "lightning_off" in msg.payload:
         pin_lightning.write(0)
 
+######################## PY Initialize ########################
+
+pygame.init()
+pygame.font.init()
+print("Validator!")
+
+screen_width = 1000
+screen_height = 1280
+photo_size = 400
+photo_ratio = 536/820
+photo_x = 200
+photo_y = 200
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.NOFRAME);
+area_photo = pygame.Rect(200,200,600,811)
+photo_box = screen.subsurface(area_photo)
+area_text = pygame.Rect(50, 50, photo_x, photo_y)
+#print(screen)
+#print(area_text)
+textBox = screen.subsurface(area_text)
+textBox.fill(white)
+
+######################## MQTT Initialize ########################
+topic        = "escapee/validator"
+topicAll     = topic + "/#"
+topicStatus  = topic + "/status"
+topicCommand = topic + "/command"
+topicTimer   = topic + "/timer"
+
+#broker="192.168.56.220"
+broker_address="192.168.56.220" 
+port=1883
+client = mqtt.Client("client") 
+client.on_connect = on_connect
+client.message_callback_add(topicCommand, on_message_command)
+client.on_message = on_message
+client.connect(broker_address, 1883, 60) #connect to broker
+client.loop_start()
+client.publish(topicStatus,"Starting")#publish
+time.sleep(1) #slight delay to show starting status
+client.publish(topicStatus,"Waiting")
+
 
 ######################## Main ########################
 def main():
@@ -224,7 +226,7 @@ def main():
 
     #wait until key switch is turned on
     while powered_off == True:
-        while pin_keyswitch.read() == 0:
+        #while pin_keyswitch.read() == 0:
             powered_off = False
     
     powerupanalyzer()
@@ -248,7 +250,6 @@ def main():
 
     powerupgenerator()
     #message_display("Sending sample to generator....","Sending sample to generator")
-
 
     quit()
 
